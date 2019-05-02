c1a063eac8dcd7fb638d9f3367771cb6a0b75291
Brandon Evans <43584979+brandonTheProgram@users.noreply.github.com>
2019-05-01 21:32:30 -0700

    Added the ability to add and delete shapes to the render area

---
+++ ExampleProjectShapes(V3.2)/mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QComboBox>
+#include <QCheckBox>
+#include <QSpinBox>
+#include <QLabel>
+#include <QLayout>
+#include <QInputDialog>
+#include <vector.h>
+#include "RenderArea.h"
+#include "createshape.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void shapeChanged();
+    void penChanged();
+    void brushChanged();
+
+    void GenerateShape(const newShapeInfo&);
+
+    void on_actionNew_Shape_triggered();
+
+    void on_actionDelete_Shape_triggered();
+
+    void on_actionMove_Shape_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    RenderArea *renderArea;
+    CreateShape *createShape;
+};
+
+#endif // MAINWINDOW_H
